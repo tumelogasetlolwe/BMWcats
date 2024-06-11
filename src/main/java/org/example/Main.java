bdf9c92803f8d90c98acc8048d3f09087091f01d
tumelogasetlolwe <ttyeyo@gmail.com>
2024-06-11 18:29:56 +0200

    first commit upstream

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Tumelo Geee");
+    }
+
+    public String Nhlanhla(){
+
+
+        return null;
+    }
+
+    public String Teee(){
+
+        return null;
+    }
+}
